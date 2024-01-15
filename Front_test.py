19ef1114516ef8107d5063bb298f207f6c83fa6c
photop33 <75907575+photop33@users.noreply.github.com>
2024-01-15 10:56:44 +0200

    Update Front_test.py

--- Front_test.py
+++ Front_test.py
@@ -2,7 +2,6 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import time
 
-# Set the path to your ChromeDriver executable
 chromedriver_path = 'https://github.com/photop33/Proceed-Test/chromedriver.exe'  # Change this to the actual path
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_argument(f"executable_path={chromedriver_path}")
@@ -13,14 +12,12 @@ password='12345'
 driver.get("http://localhost:5000")
 time.sleep(5)
 
-# Find the input element by XPath
 driver.find_element("xpath", '//*[@id="username"]').send_keys(username)
 driver.find_element("xpath", '//*[@id="password"]').send_keys(password)
 driver.find_element("xpath", '/html/body/form/input[3]').click()
 time.sleep(2)
 current_url = driver.current_url
 
-# Print or use the URL as needed
 print("Current URL:", current_url)
 target_url = 'http://localhost:5000/enable-mfa/' + username
 try:
